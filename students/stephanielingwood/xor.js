e76fb87b9beb89631e509cf178566c0c3a57fb77
Stephanie Lingwood <stephanielingwood@gmail.com>
2014-07-07 22:32:50 -0700

    removed console.logs

--- students/stephanielingwood/xor.js
+++ students/stephanielingwood/xor.js
@@ -1,14 +1,11 @@
 var preferredName = function (FirstName,LastName) {
     if (FirstName && !LastName) {
-        console.log(FirstName);
         return FirstName
     }
     else if (!FirstName && LastName) {
-        console.log(LastName);
         return LastName
     }
     else if (FirstName && LastName) {
-        console.log("false");
         return false;
     }
     else {
