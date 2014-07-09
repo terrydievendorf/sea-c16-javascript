8c0dba46d25ccf764f21acccd3cec01a9ca24609
Mandi Grant <mandi018@gmail.com>
2014-07-08 17:22:37 -0700

    Shortened code into fewer lines and added link to a fiddle demo.

--- students/mandigrant/mjgrant_assignment1_xor.js
+++ students/mandigrant/mjgrant_assignment1_xor.js
@@ -1,12 +1,8 @@
-var preferredName = function (firstName,lastName) {
-	if (firstName || lastName) {
-		return ((firstName && lastName) ? false : firstName || lastName);
-	} else {
-		return false;
-	}
+function preferredName (firstName, lastName) {
+    return (!firstName == !lastName) ? false : (firstName || lastName);
 };
 //if a first name and a last name are given, return false
 //if a first name OR last name is given, return that name
 //if no names are given, return false
 
-//gist with interactive demo: https://gist.github.com/MJGrant/835e587a6950e77f9b89
+//fiddle with test cases logged to console: http://jsfiddle.net/pBQ5p/
