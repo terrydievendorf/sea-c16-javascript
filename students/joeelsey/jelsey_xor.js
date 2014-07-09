a04add07e462b7654616d806ad667efedb9110ff
joeelsey <joe.elsey@gmail.com>
2014-07-09 09:33:16 -0700

    added second function

--- students/joeelsey/jelsey_xor.js
+++ students/joeelsey/jelsey_xor.js
@@ -4,6 +4,16 @@ var FirstName, LastName;
 var preferredName = function(FirstName,LastName) {
   if (FirstName && LastName) || (!FirstName && !LastName){
   return false;
-} else {
+  } else {
   return FirstName || LastName;
+  }
 }
+
+var preferredName1 = function(FirstName,LastName) {
+  if !(FirstName || LastName) {
+    return false;
+  }  else {
+    return FirstName || LastName
+  }
+}
+ 
