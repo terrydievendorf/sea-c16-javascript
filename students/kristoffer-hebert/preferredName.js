2c88bfd35e645ca2b70b1d4337fc5e646c263bef
Kristoffer Hebert <khebert@kristoffers-mbp.hsd1.wa.comcast.net>
2014-07-09 11:22:40 -0700

    Added preferredName assignment

---
+++ students/kristoffer-hebert/preferredName.js
@@ -0,0 +1,37 @@
+(function () {
+
+    'use strict';
+
+    var Person,
+        person1,
+        person2,
+        person3;
+
+    Person = function (first, last) {
+        var self = this;
+        self.first = first;
+        self.last = last;
+        self.isValidUsername = function () {
+
+            if ((self.first && self.last) || self.first) {
+                if (self.last) {
+                    console.log('Welcome ' + self.first + ' ' + last);
+                } else {
+                    console.log('Welcome ' + self.first + ' Please provide a last name');
+                }
+                return true;
+            }
+            console.log('Invalid preferred name');
+            return false;
+        };
+    };
+
+    person1 = new Person(null, 'Hebert');
+    person2 = new Person('Kristoffer', 'Hebert');
+    person3 = new Person('Kristoffer', null);
+
+    person1.isValidUsername();
+    person2.isValidUsername();
+    person3.isValidUsername();
+
+}());
