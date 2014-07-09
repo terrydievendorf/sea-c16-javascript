07a9e29e8dfe01d44a35b36276ca9832a3fc2f95
Nicholas Weber <nicholaseweber@gmail.com>
2014-07-08 23:00:41 -0700

    Corrected logic all-blank or all-filled and finished program.

--- students/nicholasweber/xor.js
+++ students/nicholasweber/xor.js
@@ -1,7 +1,18 @@
 var preferredName = function(firstName, lastName) {
-	if((!firstName && !lastName)) || (firstName || lastName)) {
-		return false
-	}
-	else if ()
+  if (typeof firstName != "string" || typeof lastName != "string") {
+    return console.log("Uh, something's wrong, both inputs should be strings (even if blank).");
+  }
+
+  else if ((!firstName && !lastName)) || (!!firstName && !!lastName)) {
+      return false;
+  }
+  
+  else if (!!firstName) {
+    return firstName;
+  }
+
+  else {
+    return lastName;
+  }
 
 }
