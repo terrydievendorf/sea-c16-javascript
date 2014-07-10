55874606dd139ef370384bc2608841732a576dd8
bparrish206 <brentparrish76@gmail.com>
2014-07-09 21:56:50 -0700

    updated assignment 2.2

--- students/brentParrish/preferredName.js
+++ students/brentParrish/preferredName.js
@@ -1,28 +1,12 @@
 
 var preferredName = function(FirstName, LastName) {
-	if (FirstName && LastName) {
-		return false
-	}
-	else if (FirstName && !LastName) {
+    if (FirstName && !LastName) {
 		return FirstName
 	}
 	else if (LastName && !FirstName) {
 		return LastName
 	}
-
 	else { return false};
 
 	}
-var FirstName,
-    LastName;
-
-console.log(preferredName(FirstName, LastName));
-
-Fname = preferredName("Brent", LastName);
-console.log(Fname);
-
-Lname = preferredName(FirstName, "Parrish");
-console.log(Lname);
-
-console.log(preferredName(FirstName, LastName));
 
