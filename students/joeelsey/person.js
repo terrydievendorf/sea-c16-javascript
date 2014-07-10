baa043c7478f3649ee210cf75be746722a173e51
joeelsey <joe.elsey@gmail.com>
2014-07-09 20:45:03 -0700

    submitting homework and homework changes

--- students/joeelsey/person.js
+++ students/joeelsey/person.js
@@ -2,13 +2,16 @@ var person = function(name,age,parentsName) {
 	this.name = name;
 	this.age = age;
 	this.parentsName = parentsName;
-	this.isDead = function () {
-		if (typeof(this.age) == 'number') {
-			return true;
-		} else {
-			return false;
-		}
-	}
+  this.isDead = function () {
+    typeof(this.age) == 'number' ? true : false; 
+  }
+}
+
+person.prototype.isAlive = function() {
+    if (this.age >= 1 && this.age <= 120) {
+      return true;
+    }
 }
 
 var joe = new person('joe',35,'john');
+joe.isAlive();
---
+++ students/joeelsey/seperator.js
@@ -0,0 +1,14 @@
+
+var seperator = function(input,filter) {
+	input = [];
+	for (var i = 0; i <= input.length; i++) {
+		if (filter == input[i]) {
+			input.splice(0,filter,['']);
+			console.log(input);
+		} else {
+			return true
+		}
+	}
+};
+
+seperator('asdf$lskd1234$asdo$$$','$');
