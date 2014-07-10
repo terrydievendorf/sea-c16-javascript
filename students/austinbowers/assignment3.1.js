57da3a714355e14f3da572cedc6c4a60dcf372a2
Austin Bowers <bowers.austin@gmail.com>
2014-07-10 12:23:30 -0700

    Added True & False returns for conditional statement removed age argument from isDead function

--- students/austinbowers/assignment3.1.js
+++ students/austinbowers/assignment3.1.js
@@ -2,11 +2,13 @@ var Person = function (name, parentName, age) {
 	this.name = name
 	this.parentName = parentName
 	this.age = age
-	this.isDead = function (age) {
+	this.isDead = function () {
 		if (!this.age) {
-			console.log("Well, thats awkward, " +name + " apears to be dead....");
+			console.log("Well, thats awkward, " +this.name + " apears to be dead....");
+      return true;
 		} else {
-          console.log(this.name + " is alive!")
-        }
+          console.log(this.name + " is alive!");
+          return false;
+    }
 	}
 };
