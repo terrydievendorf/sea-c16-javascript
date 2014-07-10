ae930fb5f35fe6b314e5a7c1361b098499ff408f
Jarrod Medrano <jmedran@gmail.com>
2014-07-09 20:40:52 -0700

    removing other stuff

--- students/jarrodmedrano/3-functions-objects/person.js
+++ students/jarrodmedrano/3-functions-objects/person.js
@@ -2,12 +2,6 @@ var Person = function(fname, parentname, age) {
 	this.fname = fname;
 	this.parentname = parentname;
 	this.age = age;
-	if (typeof fname === 'undefined') {
-        throw new TypeError("'fname' is required!");
-    }
-    if (typeof parentname === 'undefined') {
-        throw new TypeError("'parentname' is required!");
-    }
 	this.isDead = function() {
 		if(this.age >= 115 && this.age < -1 ) {
 			return true;
