abd47d99b04e69ce6d5c28ab5c03dafcd6e26430
Sean <Sean@Daniels-MacBook-Pro.local>
2014-07-09 18:44:56 -0700

    Removed extra content from stringDelimiter.js

--- students/sean_robertson/stringDelimiter.js
+++ students/sean_robertson/stringDelimiter.js
@@ -1,11 +1,3 @@
-// Write a function that takes in a string seperated by a seperator, and will return an array 
-// of strings in between the seperators.
-
 var stringDelimiter = function(string, seperator) {
     return string.split(seperator);
-} 
-
-var sampleInput = 'asdf$lskd1234$asdo$$$';
-
-stringDelimiter(sampleInput, '$');
-// -> ['asdf', 'lskd1234', 'asdo']
+}
