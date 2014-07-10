5f72b96099c526141820004619ae2e84c36a4b45
vernamCipher0101 <vernamCipher0101@users.noreply.github.com>
2014-07-09 21:09:08 -0700

    Update xor.js

--- students/timothyKang/xor.js
+++ students/timothyKang/xor.js
@@ -1,11 +1,11 @@
 function preferredName (FirstName, LastName) {
 	if (!FirstName && !LastName) {
-		console.log(false);
+		return false;
 	} else if (FirstName && !LastName) {
-		console.log(FirstName);
+		return FirstName;
 	} else if (FirstName && LastName) {
-		console.log(false);
+		return false;
 	} else if (!FirstName && LastName){
-		console.log(LastName);
+		return LastName;
 	}
 }
