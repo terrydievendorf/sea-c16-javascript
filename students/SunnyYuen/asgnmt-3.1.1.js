0966ac881c07a036b60fa0908b31052c2e9b4da7
yuens1002 <yuens1002@gmail.com>
2014-07-11 09:10:37 -0700

    Assignment 3.1 Update2

--- students/SunnyYuen/asgnmt-3.1.1.js
+++ students/SunnyYuen/asgnmt-3.1.1.js
@@ -1,10 +1,18 @@
 var Person = function(name, parentName, age) {
   this.name = name;
   this.parentName = parentName;
-  this.age = age;
-  
+  // in case of accidental '#' entry
+  age = parseInt(age, 10);
+  // prevents this.age assignment if age is not a number. Unfortunately,
+  // isDead or isLive methods can work without age being defined.
+  // In a real world situation, the user will never get to the isDead or isLive
+  // methods since it wouldn't pass this js test
+  return isNaN(age) ? console.log('no age is entered or is not a number') : this.age = age;
+
 };
 
+//method to see if the person is dead
+//assuming 114 is the oldest age possible and 0 is unborn
 Person.prototype.isDead = function() {
    if (this.age > 114 || this.age <= 0) {
       return true;
@@ -12,7 +20,8 @@ Person.prototype.isDead = function() {
       return false;
     }};
 
-Person.prototype.isAlive = function() {
+//same assumptions as above
+Person.prototype.isLive = function() {
    if (0 < this.age < 115) {
       return true;
     } else {
