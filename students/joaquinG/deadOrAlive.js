234e02663164c3e275859e96baf2e159e6c2b3b5
macikokoro <betwinsouls@gmail.com>
2014-07-11 00:37:12 -0700

    Added a parent name and isDead method

--- students/joaquinG/deadOrAlive.js
+++ students/joaquinG/deadOrAlive.js
@@ -1,22 +1,32 @@
-function Person(name, age) {
+function Person(name, age, parent) {
     this.name = name;
     this.age = age;
+    this.parent = parent;
+    
     this.sayHi = function() {
         return "Hi I am " + this.name + " " + this.age;
     };
     
     this.isAlive = function() {
-        if(this.age < 113) {
-            return this.name + " is Alive";
+        if(this.age < 123) {
+            return true;
         } else {
-            return this.name + " is Dead";
+            return isDead();
+        }
+    };
+
+    this.isDead = function() {
+        if(this.age > 123 || this.age < 0) {
+            return true;
         }
     };
 }
-
-var person1 = new Person("Bob", 45);
-var person2 = new Person("Bugs", 113);
+//test cases
+var person1 = new Person("Bob", 45, "Brother Bear");
+var person2 = new Person("Bugs", 122);
+var person3 = new Person("Scarface", 124);
 
  
 console.log(person1.sayHi() + " " + person1.isAlive());
 console.log(person2.sayHi() + " " + person2.isAlive());
+console.log(person3.sayHi() + " " + person3.isDead());
