bfa9b47a5d801088feeb0bd9caef72d3e865bb46
Scott Burbidge <scottisreally@gmail.com>
2014-07-10 20:22:52 -0700

    Assn. 3.1

--- students/scottBurbidge/Assignment 3.1.js
+++ students/scottBurbidge/Assignment 3.1.js
@@ -1,9 +1,23 @@
- var name = 'james';
-var parent = 'Julie';
-var age = 15;
 var Person = function(name, parent, age) {
     this.name = name;
     this.parent = parent;
-    this.age = age;
-  };
-console.log(this.name, this.parent, this.age);
+    this.age = age
+    this.isDead = function(age) {
+      if(!this.age){
+        console.log('You outside of time and space or somethimng?');
+       } else {
+         if(this.age <= 80){
+           console.log('You seem to be telling the truth.');
+         } else {
+           console.log('Is that possible?');
+         }
+       }
+    };
+}; 
+
+
+p = new Person("clean", "crate", 22);
+
+
+    
+console.log(p.name, p.parent, p.age, p.isDead());
