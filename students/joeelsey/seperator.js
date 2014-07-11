db30494baffd08f2fc50baf1e58e6317dd884006
joeelsey <joe.elsey@gmail.com>
2014-07-11 12:49:57 -0700

    Homework for monday done.

--- students/joeelsey/seperator.js
+++ students/joeelsey/seperator.js
@@ -1,14 +1,9 @@
 //Work in progress.
-var seperator = function(input,filter) {
-	input = [];
-	for (var i = 0; i <= input.length; i++) {
-		if (filter == input[i]) {
-			input.splice(0,filter,['']);
-			console.log(input);
-		} else {
-			return true
-		}
-	}
-};
 
-seperator('asdf$lskd1234$asdo$$$','$');
+var delimiter = function(input,limit) {
+  var output = input.split(limit);
+  var newoutput = output.filter(Boolean);
+  console.log(newoutput);
+}
+
+delimiter('asdf$lskd1234$asdo$$$','$');
