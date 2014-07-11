52bb0ea16297c9110906e93a3dea94e42962eade
Scott Burbidge <scottisreally@gmail.com>
2014-07-10 22:11:57 -0700

    First submission for 3.1

--- students/scottBurbidge/Assignment 3.1.js
+++ students/scottBurbidge/Assignment 3.1.js
@@ -1,23 +1,19 @@
-var Person = function(name, parent, age) {
-    this.name = name;
-    this.parent = parent;
-    this.age = age
-    this.isDead = function(age) {
-      if(!this.age){
-        console.log('You outside of time and space or somethimng?');
-       } else {
-         if(this.age <= 80){
-           console.log('You seem to be telling the truth.');
-         } else {
-           console.log('Is that possible?');
-         }
-       }
-    };
-}; 
-
-
-p = new Person("clean", "crate", 22);
-
-
-    
-console.log(p.name, p.parent, p.age, p.isDead());
+var Person = function (name, parent, age, parentAge) {
+  this.name = name;
+  this.parent = parent;
+  this.age = age;
+  this.parentAge = parentAge;
+  this.ageCheck = function (age, parentAge) {
+    if (!this.age) {
+      console.log('You outside of time and space or something?');
+    } else {
+      if (this.age > 100) {
+        console.log('Is that possible');
+      } else if (this.age >= this.parentAge) {
+        console.log("I'm not sure how that could happen, it must be complicated though");
+      } else if (this.age <= 100) {
+        console.log('That\'s a good age');
+      }
+    }
+  };
+};
