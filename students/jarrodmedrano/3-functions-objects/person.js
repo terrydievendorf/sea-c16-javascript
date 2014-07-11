888305dab215e5729802436d0777216345165db7
Jarrod Medrano <jmedran@gmail.com>
2014-07-11 11:35:26 -0700

    checking for lack of variable and for age less than or equal to zero

--- students/jarrodmedrano/3-functions-objects/person.js
+++ students/jarrodmedrano/3-functions-objects/person.js
@@ -3,7 +3,10 @@ var Person = function(fname, parentname, age) {
 	this.parentname = parentname;
 	this.age = age;
 	this.isDead = function() {
-		if(this.age >= 115 && this.age < -1 ) {
+		if(!this.age) {
+			return false;
+		}
+		else if(this.age >= 115 || this.age <= 0 ) {
 			return true;
 		} else {
 			return false;
