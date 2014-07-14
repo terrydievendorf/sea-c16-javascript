a8e550478ee7a35d498425989501bca60e5b5bfc
unknown <SeanR@Metia-Sean.seattle.circus.com>
2014-07-14 12:27:36 -0700

    Made changes to stringDelimiter.js

--- students/sean_robertson/stringDelimiter.js
+++ students/sean_robertson/stringDelimiter.js
@@ -1,3 +1,11 @@
 var stringDelimiter = function(string, seperator) {
-    return string.split(seperator);
+    var oldStringArray = string.split(seperator);
+    var newStringArray = [];
+    
+    for (var i = 0; i < oldStringArray.length; i++) {
+        if (oldStringArray[i]) {
+            newStringArray.push(oldStringArray[i]);
+        }
+    }
+    return newStringArray;
 }
