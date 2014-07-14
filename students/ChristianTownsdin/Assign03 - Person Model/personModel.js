51ca07b9cf3a3da1fd515db3857cab97b166701b
CTownsdin <Townsdin@gmail.com>
2014-07-14 16:56:28 -0700

    Added personModel.js

---
+++ students/ChristianTownsdin/Assign03 - Person Model/personModel.js
@@ -0,0 +1,23 @@
+/**
+Person Model
+A classification of an object that describes a person in Javascript. 
+It's constructor should take a name, parent name, and optional age. The object 
+should have a #isDead method that returns a boolean based on if the age is 
+set or not.
+**/
+var Person = function(name, parentName, age){
+	this.name = name;
+	this.parentName = parentName;
+	this.age = age;
+	this.isDead = function(){
+		// no one lives forever, 114 is max age allowed
+		if (this.age >= 115 || this.age <= 0){
+			return true;
+		}
+		else {
+			return false;
+		}
+	};	
+};
+
+	
