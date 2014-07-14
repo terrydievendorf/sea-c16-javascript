172e5c9a13928d7b022f7563864ed9a651f2ddd9
CTownsdin <CTownsdin@users.noreply.github.com>
2014-07-14 13:14:37 -0700

    Update preferredName.js

--- students/ChristianTownsdin/Assign02 - XOR - preferredName()/preferredName.js
+++ students/ChristianTownsdin/Assign02 - XOR - preferredName()/preferredName.js
@@ -1,10 +1,6 @@
 // this function implements XOR
-function preferredName(firstName, lastName){
-	
-	if ( !!firstName != !!lastName){
-		// notice, both: T,T  neither: F,F  just1st: T,F  just2nd: F,T
-		// SO, we have one or the other, but not both or neither :)
-		return firstName + lastName
-	}
+function preferredName(firstName, lastName){	
+	if (firstName && !lastName){ return firstName }
+	else if (!firstName && lastName){ return lastName }
 	else return false;
-}
+};
