66b46b517439a681e8823ba1e1a0daedde8312ac
lwscroggins <larry.scroggins@gmail.com>
2014-07-14 14:58:51 -0700

    Assignment 4.1 String Delimiter code

---
+++ students/larryscroggins/Assignment_4_1.js
@@ -0,0 +1,10 @@
+var sampleInput = 'asdf$lskd1234$asdo$$$';
+var stringSeparator;
+
+stringDelimiter = function(sampleInput, stringSeparator) {
+  var arr = sampleInput.split(stringSeparator);
+  return arr.filter(function(e) {return e});
+}
+
+stringDelimiter(sampleInput, '$');
+// -> ['asdf', 'lskd1234', 'asdo']
