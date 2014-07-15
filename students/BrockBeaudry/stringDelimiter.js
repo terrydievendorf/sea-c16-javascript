0d0ce01b74d80a31d1cbf39d6894585e8f724930
BrockBeaudry <brock.beaudry@gmail.com>
2014-07-15 11:41:47 -0700

    Refactor return value evaluation to be more concise.

--- students/BrockBeaudry/stringDelimiter.js
+++ students/BrockBeaudry/stringDelimiter.js
@@ -4,8 +4,6 @@ var stringDelimiter = function(input, delimiter) {
     // Match everything not in the negated character set.
     var regexp = new RegExp('[^' + delimiter + ']+', 'g');
 
-    var result =  input.match(regexp);
-
     // Return false instead of null if there are no matches.
-    return result === null ? false : result;
+    return input.match(regexp) || false;
 };
