29019c04ca3237943204610a0e248a16a555d089
lwscroggins <larry.scroggins@gmail.com>
2014-07-14 23:36:15 -0700

    Updated Assignment 4.1 to comply with secret instructions

--- students/larryscroggins/Assignment_4_1.js
+++ students/larryscroggins/Assignment_4_1.js
@@ -1,9 +1,15 @@
 var sampleInput = 'asdf$lskd1234$asdo$$$';
 var stringSeparator;
+var array = [];
 
 stringDelimiter = function(sampleInput, stringSeparator) {
-  var arr = sampleInput.split(stringSeparator);
-  return arr.filter(function(e) {return e});
+  var match;
+  for (i = 0; i <= sampleInput.length; i++) {
+    if (sampleInput.charAt(i) === '$') {
+      var static = i;
+      array.push(sampleInput.substring(match, i));
+      match = static+1;
+    }
+  }
+  array = array.filter(function(e) {return e});
 }
-
-stringDelimiter(sampleInput, '$');
