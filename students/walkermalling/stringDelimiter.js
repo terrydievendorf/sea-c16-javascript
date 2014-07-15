1369217c8eeb1c9937919e53982564c3cfe04f9d
Walker Malling <walker@coda.io>
2014-07-14 21:21:08 -0700

    just some clean up, no structural changes

--- students/walkermalling/stringDelimiter.js
+++ students/walkermalling/stringDelimiter.js
@@ -1,22 +1,20 @@
 var stringDelimiter = function(string, delimiter){
 	// construct
-	this.string = typeof string === "string" ? string : false;
-	this.delimiter = delimiter || ' ';
+	var string = typeof string === "string" ? string : false;
+	var delimiter = delimiter || ' ';
 
 	// validate
-	if( !this.string ){
-		throw new UserException("NoStringToDelimit");
-	}
+	if( !string ) throw new UserException("NoStringToDelimit");
 
 	// delimit
 	var delimit = [-1];
-	
-	for(var k = 0; k < this.string.length; k++){
-		if( this.string[k] === this.delimiter ) delimit.push(k); // grab position of delimiters
+
+	for(var k = 0; k < string.length; k++){
+		if( string[k] === delimiter ) delimit.push(k); // grab position of delimiters
 	}
 
 	for(var q = 0; q < delimit.length; q++){
-		delimit[q] = this.string.slice( delimit[q] + 1, delimit[q+1] ); // slice given string
+		delimit[q] = string.slice( delimit[q] + 1, delimit[q+1] ); // slice given string
 	}
 
 	return delimit;
