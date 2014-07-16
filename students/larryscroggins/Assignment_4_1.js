81afc06eba61d1ac41e402b49c27497a76d5ce44
lwscroggins <larry.scroggins@gmail.com>
2014-07-16 14:34:57 -0700

    updated to fix string not ending with stringSeparator

--- students/larryscroggins/Assignment_4_1.js
+++ students/larryscroggins/Assignment_4_1.js
@@ -4,15 +4,19 @@ var array = [];
 
 stringDelimiter = function(sampleInput, stringSeparator) {
   var match;
-  for (i = 0; i <= sampleInput.length; i++) {
+  var len = sampleInput.length;
+  for (i = 0; i <= len; i++) {
     if (sampleInput.charAt(i) === stringSeparator) {
       var static = i;
       var arrElement = sampleInput.substring(match, i);
       if (arrElement !== "") {
         array.push(arrElement);
       }
-      match = static+1;
+      match = static + 1;
     }
+  }
+  if (sampleInput.charAt(len) != stringSeparator) {
+    array.push(sampleInput.substring(match, len));
   }
   return array;
 }
