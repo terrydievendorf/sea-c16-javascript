801d9ebc8ac37123883a10abc63f985d382bdd89
yuens1002 <yuens1002@gmail.com>
2014-07-17 07:18:39 -0700

    Assignment 4.1.2

--- students/SunnyYuen/asgnmt-4.1.js
+++ students/SunnyYuen/asgnmt-4.1.js
@@ -1,15 +1,19 @@
+//found '$' can not be used as a delimiter
 function makeList(str, delimiter) {
-var array = [];
-  //define the delimiter to match
-var found = RegExp(delimiter, 'g');
-  // number of times the delimiter is found
-var occurance = (str.match(found)).length;
-for (var i = 0; i < occurance; i++) {
-  var foundIndex = str.match(delimiter);
-  var chunk = str.slice(0, foundIndex.index);
-  // slice str at +1 to skip over the delimiter
-  str = str.slice(foundIndex.index+1);
-  array.push(chunk);
+    // pervents an empty array value if the delimiter is the 1st character of the string
+  (str.indexOf(delimiter) == 0) ? str = str.slice(1) : str = str;
+  var array = [];
+    //define the delimiter to match
+  var found = RegExp(delimiter, 'g');
+    // number of times the delimiter is found
+  var occurance = (str.match(found)).length;
+  for (var i = 0; i < occurance; i++) {
+    // match function has a .index property  
+    var foundIndex = str.match(delimiter);
+    var chunk = str.slice(0, foundIndex.index);
+    // slice str at +1 to skip over the delimiter
+    str = str.slice(foundIndex.index+1);
+    array.push(chunk);
 }
 //adds the last chunk if not empty  
 if (str !== '') {array.push(str);}
