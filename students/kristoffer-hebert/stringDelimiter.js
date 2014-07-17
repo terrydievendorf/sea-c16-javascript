f7f7f63e2968f2d3249c3d9d45c62cf53df98dd9
Kristoffer Hebert <khebert@Kristoffers-MacBook-Pro.local>
2014-07-16 20:43:13 -0700

    Updated stringDelimiter to fix bugs

--- students/kristoffer-hebert/stringDelimiter.js
+++ students/kristoffer-hebert/stringDelimiter.js
@@ -2,20 +2,22 @@
     'use strict';
 
     function stringDelimiter(string, seperator) {
-        var index, array = [];
+        var index, array = [], seperatorLength = seperator.length, term;
         if (string && seperator) {
-            while (string) {
+            while (string && index !== string.length) {
                 index = string.indexOf(seperator);
-                if (index === -1) {
+                if (index == -1) {
                     index = string.length;
                 }
-                array.push(string.substring(0, index));
-                string = string.substring(index + seperator.length);
+                term = string.substring(0, index);
+                if(term) {
+                  array.push(term);
+                }
+              string = string.substring(index + seperatorLength);
             }
         } else {
             array.push(string);
         }
         return array;
     }
-
 }());
