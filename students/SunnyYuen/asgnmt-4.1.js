52bebf5d573786befcee7c68e630a284fbbf79a3
yuens1002 <yuens1002@gmail.com>
2014-07-17 01:38:30 -0700

    Assignment 4.1.1

--- students/SunnyYuen/asgnmt-4.1.js
+++ students/SunnyYuen/asgnmt-4.1.js
@@ -1,6 +1,17 @@
-var sampleInput = 'asdfre&addf1&abad&;asj;flajsdf';
-
-function stringDelimiter(sampleInput, delimiter) {
-  var arr = sampleInput.split(delimiter);
-  return arr;
-} 
+function makeList(str, delimiter) {
+var array = [];
+  //define the delimiter to match
+var found = RegExp(delimiter, 'g');
+  // number of times the delimiter is found
+var occurance = (str.match(found)).length;
+for (var i = 0; i < occurance; i++) {
+  var foundIndex = str.match(delimiter);
+  var chunk = str.slice(0, foundIndex.index);
+  // slice str at +1 to skip over the delimiter
+  str = str.slice(foundIndex.index+1);
+  array.push(chunk);
+}
+//adds the last chunk if not empty  
+if (str !== '') {array.push(str);}
+return array;
+}
