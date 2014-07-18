0a2d06d9de083e2e342b937e0aabcf890bca9ad5
macikokoro <betwinsouls@gmail.com>
2014-07-18 01:48:13 -0700

    Cleaned up for submission

--- students/joaquinG/oldest_lodashJG.js
+++ students/joaquinG/oldest_lodashJG.js
@@ -1,44 +1,7 @@
-var people = [
-  {
-    name: 'Hank',
-    age: 29,
-    parent: 'Don'
-  },
-  {
-    name: 'Deborah',
-    age: 30,
-    parent: 'Don'
-  },
-  {
-    name: 'Danny',
-    age: 24,
-    parent: 'Don'
-  },
-  {
-    name: 'Don',
-    age: 64,
-    parent: 'Haskell'
-  },
-  {
-    name: 'Kathy',
-    age: 75,
-    parent: 'Lenora'
-  },
-  {
-    name: 'Haskell',
-    parent: 'Finus'
-  },
-  {
-    name: 'Finus'
-  }
-];
-
 //function that returns the oldest age
 var oldestAge = function (people) {
   return _.max(people, 'age').age;
 };
-
-console.log(oldestAge(people));
 
 //function that returns an array of the names 
 //of all living people
@@ -49,12 +12,10 @@ var living = function (people) {
   .value();
 };
 
-console.log(living(people));
-
 //function that returns the name of the oldest living person
 var oldestLivingPerson = function (people) {
   return _.max(people, 'age').name;
 };
 
-console.log(oldestLivingPerson(people));
+
 
