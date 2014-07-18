28a1f1bc6536110303c724b3ee3fcdc082e05463
Walker Malling <walker@coda.io>
2014-07-17 19:52:07 -0700

    add doc blocks

--- students/walkermalling/lodashAssignment.js
+++ students/walkermalling/lodashAssignment.js
@@ -1,9 +1,21 @@
+/**
+ * Get a subset of the collection given a filter key
+ * 
+ * @param  collection   the colleciton of people
+ * @param  filter       the desired key to filter for
+ * @return [{}]         an array of people objects
+ */
 var filterPeople = function(collection, filter){
     var newCollection = _.filter(collection,function(record){
       return !!record[filter];
     });
 }
 
+/**
+ * Get the record of the oldest person in a collection
+ * @param  collection   the colleciton of people
+ * @return {}           object record of the person
+ */
 var oldestPerson = function(collection){
     // filter out records with no age
     var peopleWithAges = filterPeople(collection,'age');
@@ -16,11 +28,19 @@ var oldestPerson = function(collection){
     return oldest;
 }
 
+/**
+ * return the oldest age in a collection
+ * 
+ */
 var oldestAge = function(collection){
    var oldest = oldestPerson(collection);
    return oldest.age;   
 }
 
+/**
+ * return an array of the names of the living people
+ * in a given collection
+ */
 var livingPeople = function(collection){
     // filter out people with no age
     var living = filterPeople(collection,'age');
@@ -31,7 +51,10 @@ var livingPeople = function(collection){
     return names;
 }
 
-
+/**
+ * return the name of the oldest person
+ * in a given collection
+ */
 var oldestLivingPerson = function(collection){
     var oldest = oldestPerson(collection);
     return oldest.name;
