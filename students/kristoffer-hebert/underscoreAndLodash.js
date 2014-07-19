505d5426352b001aa4fd1ac5a40fa000080a0a10
Kristoffer Hebert <khebert@Kristoffers-MacBook-Pro.local>
2014-07-18 21:24:37 -0700

    Added underscore/lodash assignment

---
+++ students/kristoffer-hebert/underscoreAndLodash.js
@@ -0,0 +1,26 @@
+(function () {
+    'use strict';
+    function oldestAge(people) {
+        var result;
+        result = _.max(people, function (person) {
+            return person.age;
+        });
+        return result.age;
+    }
+
+    function living(people) {
+        var result;
+        result = _.filter(people, function (person) {
+            return person.age;
+        });
+        return result;
+    }
+    function oldestLivingPerson(people) {
+        var result;
+        result = living(people);
+        result = _.max(people, function (person) {
+            return person.age;
+        });
+        return result.name;
+    }
+}());
