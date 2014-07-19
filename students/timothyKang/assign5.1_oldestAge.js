4630901b8ccb77727a95028213306de808cf61bb
vernamCipher0101 <timothykang@Timothys-MacBook-Pro.local>
2014-07-19 15:28:45 -0700

    Assign 5.1 HW merge

---
+++ students/timothyKang/assign5.1_oldestAge.js
@@ -0,0 +1,32 @@
+var _ = require('lodash');
+
+
+function oldestAge(people) {
+		//console.log(_.max((_.pluck(people, 'age'));
+	var old = _.max(people, 'age');
+	var newOld = _.omit(old, 'name', 'parent');
+	var result = _.values(newOld);
+	return parseInt(result);
+}
+
+
+
+function living(people) {
+	var living = _.filter(people, 'age');
+	return _.pluck(living, 'name');		
+}
+
+
+
+function oldestLivingPerson(people) {	
+	var old = _.max(people, 'age');
+	var newOld = _.omit(old, 'age', 'parent');
+	var result = _.values(newOld);
+	return result.toString();
+}
+
+
+
+
+
+
