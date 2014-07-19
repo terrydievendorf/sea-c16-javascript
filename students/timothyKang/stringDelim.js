b700b1a4882213ae1c67a3fe3d476c897c1388b8
vernamCipher0101 <timothykang@Timothys-MacBook-Pro.local>
2014-07-19 13:55:41 -0700

    4.1 hw update stringDelim.js
    merge request

--- students/timothyKang/stringDelim.js
+++ students/timothyKang/stringDelim.js
@@ -1,8 +1,17 @@
+var stringArray = new Array;
+var beginArray = 0;
+
 var stringDelimiter = function (sampleInput, separator) {
 	for (var i = 0; i < sampleInput.length; i++) {
-		if (sampleInput.charAt(i) == separator) {
-			continue;
-		} 
-		console.log([sampleInput.charAt(i)]);
-	}
+    	if (sampleInput.charAt(i) == separator) {
+            myString = sampleInput.substring(beginArray,i);
+            if (myString !== '') {
+                stringArray.push(myString);
+            }
+            beginArray = i + 1;
+        }
+    }
+    console.log(stringArray);
 }
+var sampleInput = 'asdf$lskd1234$asdo$$$';
+stringDelimiter(sampleInput, '$');
