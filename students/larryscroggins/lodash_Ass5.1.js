09a32d51a6f9deb43f6e1e05367577e003912d37
lwscroggins <larry.scroggins@gmail.com>
2014-07-19 15:58:55 -0700

    added people array back in

--- students/larryscroggins/lodash_Ass5.1.js
+++ students/larryscroggins/lodash_Ass5.1.js
@@ -1,5 +1,40 @@
 var _ = require('lodash');
 
+var people = [
+  {
+    name: 'Hank',
+    age: 29,
+    parent: 'Don'
+  },
+  {
+    name: 'Deborah',
+    age: 30,
+    parent: 'Don'
+  },
+  {
+    name: 'Danny',
+    age: 24,
+    parent: 'Don'
+  },
+  {
+    name: 'Don',
+    age: 64,
+    parent: 'Haskell'
+  },
+  {
+    name: 'Kathy',
+    age: 75,
+    parent: 'Lenora'
+  },
+  {
+    name: 'Haskell',
+    parent: 'Finus'
+  },
+  {
+    name: 'Finus'
+  }
+];
+
 function oldestAge(people) {
   return _.max(people, 'age').age;
 }
