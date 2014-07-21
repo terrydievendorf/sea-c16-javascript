0f60015ab1af4027ffd591660d21be87cc744321
vernamCipher0101 <vernamCipher0101@users.noreply.github.com>
2014-07-21 16:06:41 -0700

    Updated 5.1 assign to clear jshint errors and is ready for merge

--- students/timothyKang/assign5.1_oldestAge.js
+++ students/timothyKang/assign5.1_oldestAge.js
@@ -2,25 +2,24 @@ var _ = require('lodash');
 
 
 function oldestAge(people) {
-		//console.log(_.max((_.pluck(people, 'age'));
-	var old = _.max(people, 'age');
-	var newOld = _.omit(old, 'name', 'parent');
-	var result = _.values(newOld);
-	return parseInt(result);
+  var old = _.max(people, 'age');
+  var newOld = _.omit(old, 'name', 'parent');
+  var result = _.values(newOld);
+  return parseInt(result, 10);
 }
 
 
 
 function living(people) {
-	var living = _.filter(people, 'age');
-	return _.pluck(living, 'name');		
+  var livingPeople = _.filter(people, 'age');
+  return _.pluck(livingPeople, 'name');
 }
 
 
 
-function oldestLivingPerson(people) {	
-	var old = _.max(people, 'age');
-	var newOld = _.omit(old, 'age', 'parent');
-	var result = _.values(newOld);
-	return result.toString();
+function oldestLivingPerson(people) {
+  var old = _.max(people, 'age');
+  var newOld = _.omit(old, 'age', 'parent');
+  var result = _.values(newOld);
+  return result.toString();
 }
