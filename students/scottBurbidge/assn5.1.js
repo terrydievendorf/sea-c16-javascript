e4e8d215c4731564d89a0297303eea77ca7f809e
Scott Burbidge <scottisreally@gmail.com>
2014-07-21 14:22:34 -0700

    This is assignment 5.1 first

---
+++ students/scottBurbidge/assn5.1.js
@@ -0,0 +1,18 @@
+var _ = require('lodash-node');
+
+function oldestAge(data){
+  return _.max(data, function(x){
+    return x.age;
+  });
+};
+
+function alive(data) {
+  var filtData = _.filter(data, function(nam){
+    return nam.age != undefined;
+  })
+   return _.pluck(filtData, 'name');
+};
+
+function oldName(data){
+  return oldestAge(data).name;
+};
