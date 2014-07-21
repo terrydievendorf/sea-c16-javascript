a762260293873fde360b6d6cc711c7f12b0884b2
unknown <SeanR@Metia-Sean.seattle.circus.com>
2014-07-21 16:01:22 -0700

    Changed String Delimiter

--- students/sean_robertson/stringDelimiter.js
+++ students/sean_robertson/stringDelimiter.js
@@ -1,11 +1,15 @@
 var stringDelimiter = function(string, seperator) {
-    var oldStringArray = string.split(seperator);
-    var newStringArray = [];
     
-    for (var i = 0; i < oldStringArray.length; i++) {
-        if (oldStringArray[i]) {
-            newStringArray.push(oldStringArray[i]);
+    var newString = [];
+    var startIndex = 0;
+    for (var i = 0; i < string.length; i++) {
+        if (string.charAt(i) === seperator) {
+            var subString = string.substring(startIndex,i); 
+            if (subString !== '') {
+                newString.push(subString);            
+            }
+            startIndex = i + 1;
         }
     }
-    return newStringArray;
+    return newString;
 }
