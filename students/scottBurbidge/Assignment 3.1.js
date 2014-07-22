52e000f3c06b90f6ad50cdea8c7577ff52e3a1f0
Scott Burbidge <scottisreally@gmail.com>
2014-07-22 13:54:22 -0700

    Tabs project added, minor updates to other assn

--- students/scottBurbidge/Assignment 3.1.js
+++ students/scottBurbidge/Assignment 3.1.js
@@ -4,10 +4,9 @@ var Person = function (name, parent, age, parentAge) {
   this.age = age;
   this.parentAge = parentAge;
   this.isDead = function () {
-    if (!(!this.age || this.age > 150 || this.age >= this.parentAge)) {
-      return true;
-    } else {
-      return false;
-    }
+    !(!this.age || this.age > 150 || this.age >= this.parentAge) ? true: false 
   };
 };
+
+
+
