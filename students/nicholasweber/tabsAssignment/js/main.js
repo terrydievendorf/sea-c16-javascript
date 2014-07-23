a970c38ac1dfe863e29aae0e134e806c67f41ca6
Nicholas Weber <nicholaseweber@gmail.com>
2014-07-22 21:16:19 -0700

    tightened up code.

--- students/nicholasweber/tabsAssignment/js/main.js
+++ students/nicholasweber/tabsAssignment/js/main.js
@@ -1,7 +1,6 @@
 $(document).ready
 //look up onHashChange
 $(window).on('hashchange', function (e) {
-  var displayThis = location.hash;
-  var content = $(displayThis).html();
-  $('section').html(content)
+  var content = $(location.hash).html();
+  $('section').html(content);
 });
