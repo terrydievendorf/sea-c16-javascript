b8762e9b13d8900391aa115315da104ba3078b48
jstraume <joebarrystraume@gmail.com>
2014-07-24 10:19:10 -0700

    Made code easier to read

--- students/jbarrystraume/quickOldestPerson.js
+++ students/jbarrystraume/quickOldestPerson.js
@@ -1,37 +1,11 @@
 var people = [
-  {
-    name: 'Hank',
-    age: 29,
-    parent: 'Don'
-  },
-  {
-    name: 'Deborah',
-    age: 30,
-    parent: 'Don'
-  },
-  {
-    name: 'Danny',
-    age: 24,
-    parent: 'Don'
-  },
-  {
-    name: 'Don',
-    age: 64,
-    parent: 'Haskell'
-  },
-  {
-    name: 'Kathy',
-    age: 75,
-    parent: 'Lenora'
-  },
-  {
-    name: 'Haskell',
-    parent: 'Finus'
-  },
-  {
-    name: 'Finus'
-  }
-];
+  {name: 'Hank', age: 29, parent: 'Don'}, 
+  {name: 'Deborah', age: 30, parent: 'Don'},
+  {name: 'Danny', age: 24, parent: 'Don'},
+  {name: 'Don', age: 64, parent: 'Haskell'},
+  {name: 'Kathy', age: 75, parent: 'Lenora'},
+  {name: 'Haskell', parent: 'Finus'},
+  {name: 'Finus'}];
 
 var max = -1;
 var maxName;
