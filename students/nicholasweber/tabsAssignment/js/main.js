b9b192f8c2519aa09221ad2c3e52e64ea5de2e44
Nicholas Weber <nicholaseweber@gmail.com>
2014-07-24 21:29:47 -0700

    Added functionality to change appearance of active tab.

--- students/nicholasweber/tabsAssignment/js/main.js
+++ students/nicholasweber/tabsAssignment/js/main.js
@@ -1,5 +1,8 @@
 $(document).ready
-//look up onHashChange
+$('a').on('click', function (e) {
+	$('#active-section').removeAttr('id', 'active-section');
+	$(this).attr('id', 'active-section');
+});
 $(window).on('hashchange', function (e) {
   var content = $(location.hash).html();
   $('section').html(content);
