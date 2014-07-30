04bffa43dd7b7390a66150a62f2b65c51fdbd5ba
Nicholas Weber <nicholaseweber@gmail.com>
2014-07-29 22:40:57 -0700

    Added a refresh button to relod the tabs.

--- students/nicholasweber/tabsWithAjax/js/main.js
+++ students/nicholasweber/tabsWithAjax/js/main.js
@@ -28,3 +28,8 @@ $(window).on('hashchange', function (e) {
   var content = $(location.hash).html();
   $('section').html(content);
 });
+
+$(".refresh").on('click', function() {
+  $('ul').empty();
+  populateTabs();
+})
