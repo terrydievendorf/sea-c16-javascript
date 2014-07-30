374057499815ce1b424e013245082b82d4932cf2
Nicholas Weber <nicholaseweber@gmail.com>
2014-07-29 22:33:31 -0700

    Finished up assignment.

--- students/nicholasweber/tabsWithAjax/js/main.js
+++ students/nicholasweber/tabsWithAjax/js/main.js
@@ -1,8 +1,29 @@
+/* For when I add _.template
+var _ = require('lodash');
+var hashComp = _.template()
+*/
 $(document).ready
-$('a').on('click', function (e) {
-	$('#active-section').removeAttr('id', 'active-section');
-	$(this).attr('id', 'active-section');
-});
+
+var populateTabs = function() {
+  $.getJSON('http://rs.hankyates.com:3000/content', function(data) {
+    var i = 0
+    data.forEach(function (tab, index) {
+      console.log(tab);
+      //FIXTHIS: check out _.template in lodash to do this
+      $('ul').append('<li><a href="#' + tab.name + '">' + tab.name +'</a></li>');
+      if(i==0) $("ul li a:first-child").attr('id', 'active-section');
+      $('#main-wrapper').append('<div id="' + tab.name +'">' + tab.content + '</div>')
+      if(i==0) $('section').html(tab.content);
+      i++
+      $('a').on('click', function (e) {
+        $('#active-section').removeAttr('id', 'active-section');
+        $(this).attr('id', 'active-section');
+      });
+    });
+  });
+}
+populateTabs();
+
 $(window).on('hashchange', function (e) {
   var content = $(location.hash).html();
   $('section').html(content);
